2e14eb524f9bf14574d5a7fdfe9fc8c5024440b2
haerong22 <haerong22@naver.com>
2022-04-25 10:48:47 +0900

    점프와 순간이동

---
+++ src/Algorithm79.java
@@ -0,0 +1,25 @@
+/**
+ * https://programmers.co.kr/
+ * 점프와 순간 이동
+ */
+
+public class Algorithm79 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(5000));
+    }
+
+    public static int solution(int n) {
+        int ans = 0;
+        while(n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n--;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
