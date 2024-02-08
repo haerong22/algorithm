b2d35652a583610ac542df870f474b87e0636b7c
haerong22 <haerong22@naver.com>
2024-02-08 15:45:12 +0900

    baekjoon: N과 M (3)

---
+++ src/baekjoon/P_15651.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/15651
+ *
+ * N과 M (3)
+ */
+public class P_15651 {
+
+    private static int N;
+    private static int M;
+    private static int[] selected;
+
+    private static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+
+        N = Integer.parseInt(str[0]);
+        M = Integer.parseInt(str[1]);
+        selected = new int[M + 1];
+
+        search(1);
+        System.out.println(answer.toString());
+    }
+
+    public static void search(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                answer.append(selected[i]).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            selected[k] = i;
+            search(k + 1);
+            selected[k] = 0;
+        }
+    }
+}
